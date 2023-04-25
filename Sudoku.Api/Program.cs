9ce1ddbdbfa1d66df6fa5b911ce20c59eb61f4db
wtindall1 <wtindall@hotmail.com>
2023-04-25 19:11:36 +0100

    added allow all origins

--- Sudoku.Api/Program.cs
+++ Sudoku.Api/Program.cs
@@ -2,6 +2,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowAllOrigins", policy =>
+    {
+        policy.AllowAnyOrigin();
+    });
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -14,6 +22,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseCors("AllowAllOrigins");
 }
 
 app.UseHttpsRedirection();
